7712f34062e99ab92a24c27ae45d51ecc181bc96
Katarina <zain.fake02@gmail.com>
2020-11-21 16:30:38 +0100

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,21 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
+<<<<<<< Updated upstream
     'linebreak-style': ['error', process.env.NODE_ENV === 'prod' ? 'unix' : 'windows'],
 
+=======
+    "linebreak-style": [
+      "error",
+      process.env.NODE_ENV === "prod" ? "unix" : "windows",
+    ],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+>>>>>>> Stashed changes
   },
-};
+}
